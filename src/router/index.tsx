52038a73b2b05759d5aeced088cfc66f0a974016
paul0pv <maurossdend@gmail.com>
2024-06-22 06:57:50 -0500

    responsive navigation

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { SignUpPage, LogInPage, HomePage, DashboardPage } from '../pages';
+import Profile from '../pages/Profile';
 
 const AppRouter: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const AppRouter: React.FC = () => {
         <Route path="/signup" element={<SignUpPage />}/>
         <Route path="/" element={<HomePage />}/>
         <Route path="/dashboard" element={<DashboardPage />}/>
+        <Route path='/profile' element={<Profile/>}/>
       </Routes>
     </Router>
   );
